2e7f0b754a677c9873eda588bf261b4558d9a05b
Robert Garrison <robert.garrison@unityfisolutions.com>
2019-12-09 14:02:49 -0500

    Updating to Oracle.ManagedDataAccess.Core 2.19.60 and ADONetHelper 4.0.0

--- OracleClient.cs
+++ OracleClient.cs
@@ -22,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 #endregion
 #region Using Statements
+using ADONetHelper.Core;
 using Oracle.ManagedDataAccess.Client;
 using System.IO;
 using System.Data;
